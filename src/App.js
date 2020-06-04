579d13d263642ffa21f66b6a9d2db04827e798b2
Jakob <jakob.jenko@gmail.com>
2020-06-04 17:45:37 +0200

    0.6.0

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from "./components/home"
 import Rezka from "./components/rezka"
 import Weather from "./components/weather"
 
-// uvozim navigavijo, kjer je home root fi
+// uvozim modul za navigacijo
 import NavigationBar from "./components/utils/navbars/navigationBar"
 
 function App() {
